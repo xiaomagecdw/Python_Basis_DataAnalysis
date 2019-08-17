ac3dc0da0faee5f4969bfa13b46046c969018b76
陈代武 <chendaiwu@xiaomagecdw-MacBook-Pro.local>
2019-08-18 01:42:18 +0800

    数据分析基础列表类型学习

--- Chapter1/list_script.py
+++ Chapter1/list_script.py
@@ -4,3 +4,12 @@
 # Author : chendaiwu biubiubiu.....
 # File : list_script.py
 # Sofaware : PyCharm
+
+#使用方括号创建一个列表
+#用len()计算列表中元素的数量
+#用max()和min()找出最大值和最小值
+#用count()计算除列表中某个值出现的次数
+a_list = [1, 2, 3]
+print("Output #58: {}".format(a_list))
+print("Output #59: a_list has {} elements.".format(len(a_list)))
+print("Output #60: the maximun value in a_list is {}.".format(max(a_list)))
