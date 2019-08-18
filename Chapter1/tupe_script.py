f1988503172098f13d4b0099bf425cd1932e843c
陈代武 <chendaiwu@xiaomagecdw-MacBook-Pro.local>
2019-08-19 00:59:15 +0800

    创建元组、元组解包

--- Chapter1/tupe_script.py
+++ Chapter1/tupe_script.py
@@ -4,3 +4,26 @@
 # Author : chendaiwu biubiubiu.....
 # File : tupe_script.py
 # Sofaware : PyCharm
+
+#创建元组
+#使用圆括号创建元组
+my_tuple = ('x', 'y', 'z')
+print("Output #93: {}".format(my_tuple))
+print("Output #94: my_tuple has {} elements".format(len(my_tuple)))
+print("Output #95: {}".format(my_tuple[1]))
+longer_tuple = my_tuple + my_tuple
+print("Output #96: {}".format(longer_tuple))
+
+#元组解包
+#使用赋值操作符左侧的变量对元组进行解包
+one, two, three = my_tuple
+print("Output #97: {0} {1} {2}".format(one, two, three))
+var1 = 'red'
+var2 = 'robin'
+print("Output #98: {} {}".format(var1, var2))
+
+#在变量之间变化彼此的值
+var1, var2 = var2, var1
+print("Output #99: {} {}".format(var1, var2))
+
+
