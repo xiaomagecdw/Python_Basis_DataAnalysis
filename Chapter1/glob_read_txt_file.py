e9342f12b0d9894fb3096bd3ef3813f05a54cf8e
陈代武 <chendaiwu@xiaomagecdw-MacBook-Pro.local>
2019-08-25 23:59:55 +0800

    增加data文件夹存储文件

---
+++ Chapter1/glob_read_txt_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env Python
+# -*- coding:utf-8 -*-
+# Time : 2019/8/25 23:51
+# Author : chendaiwu biubiubiu.....
+# File : glob_read_txt_file.py
+# Sofaware : PyCharm
---
+++ Chapter1/read_text_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env Python
+# -*- coding:utf-8 -*-
+# Time : 2019/8/25 23:30
+# Author : chendaiwu biubiubiu.....
+# File : read_text_file.py
+# Sofaware : PyCharm
